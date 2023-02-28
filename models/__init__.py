87de8c5c37e904132ffcc4d7497706ce4dddd26b
lizenan <lizenan1995@foxmail.com>
2023-02-28 09:59:38 +0800

    log update

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,4 @@ from .resnet import *
 from .densenet import *
 from .lenet import *
 from .vgg import *
-from .alexnet import *
-from .mobilenet import *
-from .WideResNet import *
 from .mlp import *
